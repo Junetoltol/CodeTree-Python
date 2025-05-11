c95c2002bdfef43b2cd614ce019eafebe00d5e0a
Junetoltol <113653835+Junetoltol@users.noreply.github.com>
2025-05-11 20:20:59 +0900

    Title: 숫자들의 배수, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250511/숫자들의 배수/multiple-of-numbers.py
+++ 250511/숫자들의 배수/multiple-of-numbers.py
@@ -1,17 +1,20 @@
+# 배열 선언
 arr = []
-count_5 = 0
+cnt = 0
 
-while True:
-    n = int(input())
-    if n < 10 or n > 100:
-        continue  # 조건에 맞지 않으면 다시 입력
+# 정수 한 개를 입력받아 배열의 첫 인덱스에 저장
+n = int(input())
+arr.append(n)
 
-    arr.append(n)
+# 입력받은 정수의 배수를 배열에 저장
+for i in range(1, 10):
+	a = arr[i - 1] + arr[0]
+	arr.append(a)
 
-    if n % 5 == 0:
-        count_5 += 1
-
-    if count_5 == 2:
-        break
-
-print(*arr)
+# 배열의 원소들을 출력하다가 5의 배수가 2번 나오면 출력을 멈춤
+for elem in arr:
+	print(elem, end=" ")
+	if elem % 5 == 0:
+		cnt += 1
+	if cnt >= 2:
+		break
