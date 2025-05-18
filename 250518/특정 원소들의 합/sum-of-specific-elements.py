e927889f3efe0c297e8827d1c5ff2fbd17ec7a1b
Junetoltol <113653835+Junetoltol@users.noreply.github.com>
2025-05-18 19:31:41 +0900

    Title: 특정 원소들의 합, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250518/특정 원소들의 합/sum-of-specific-elements.py
+++ 250518/특정 원소들의 합/sum-of-specific-elements.py
@@ -1,10 +1,14 @@
-# 4x4 배열 입력 받기
-arr = [list(map(int, input().split())) for _ in range(4)]
+# 2차원 배열을 구현해 각 줄마다 정수를 입력받습니다.
+arr_2d = [
+	list(map(int, input().split()))
+	for _ in range(4)
+]
+sum_val = 0
+	
+# 색칠된 칸에 있는 원소들의 합을 구합니다.
+for i in range(4):
+	for j in range(i + 1):
+		sum_val += arr_2d[i][j]
 
-# 특정 영역 합 계산: 행 1~3, 열 0~2
-total = 0
-for i in range(1, 4):         # 1행~3행 (index 1~3)
-    for j in range(0, 3):     # 0열~2열 (index 0~2)
-        total += arr[i][j]
-
-print(total)
+# 출력
+print(sum_val)
